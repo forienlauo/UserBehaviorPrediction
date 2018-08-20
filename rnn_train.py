924eacd7eb1db60fef6d434d6f6f433d73b7a768
forienlauo <244382021@qq.com>
2018-08-20 17:14:32 +0800

    Compare with pure RNN

---
+++ rnn_train.py
@@ -0,0 +1,141 @@
+# coding=utf-8
+import argparse
+import logging
+import os
+import shutil
+import sys
+
+import conf
+from src.moduler.adapter.rnn_data_adapter import \
+    RnnDataAdapter as DataAdapter, \
+    RnnData as Data
+from src.moduler.trainer.rnn_trainer import \
+    RnnTrainer as Trainer
+
+
+def __parseArgs():
+    parser = argparse.ArgumentParser(description='Train combined model')
+
+    parser.add_argument("-f", "--featureFrame3dDir", required=True, help=u"featureFrame3d dir")
+    parser.add_argument("-t", "--targetBehaviorDir", required=True, help=u"targetBehavior dir")
+    parser.add_argument("-p", "--trainExampleP", type=float, required=False, default=0.8,
+                        help=u"train example splitting probability")
+    parser.add_argument("-c", "--cacheDir", required=False, default="./.cache", help=u"cache dir")
+
+    parser.add_argument("-w", "--wkdir", required=False, default="./train_wkdir",
+                        help=u"wkdir, must exist and be empty")
+
+    parser.add_argument("-l", "--lstmSize", type=int, required=False, default=20, help=u"LSTMCell size")
+
+    parser.add_argument("-b", "--batchSizeConf", type=int, required=False, default=20, help=u"batch size configured")
+    parser.add_argument("-k", "--keepProbConf", type=float, required=False, default=0.5, help=u"keepProb configured")
+
+    parser.add_argument("-C", "--cpuCoreCnt", type=int, required=False, default=1, help=u"cpuCore cnt")
+    parser.add_argument("-G", "--gpuNos", required=False, default=None, help=u"gpu no s, separated by comma")
+    parser.add_argument("-M", "--gpuMemFraction", type=float, required=False, default=1.0,
+                        help=u"per process gpu memory fraction")
+    parser.add_argument("-I", "--iteration", type=int, required=False, default=100, help=u"train step iteration")
+    parser.add_argument("-S", "--printProgressPerStepCnt", type=int, required=False, default=10,
+                        help=u"print progress per step cnt")
+
+    parser.add_argument("-L", "--logLevel", required=False, default="info", help=u"log level")
+
+    parser.add_argument("-F", "--force", action="store_true", required=False, default=False,
+                        help=u"general force option, for example force removing wkdir is already exist")
+
+    g_options = parser.parse_args()
+    return g_options
+
+
+def main():
+    logging.info("argv: %s" % " ".join(sys.argv))
+    options = __parseArgs()
+
+    featureFrame3dDir = options.featureFrame3dDir
+    targetBehaviorDir = options.targetBehaviorDir
+    trainExampleP = options.trainExampleP
+    cacheDir = options.cacheDir
+    wkdir = options.wkdir
+
+    lstmSize = options.lstmSize
+
+    batchSizeConf = options.batchSizeConf
+    keepProbConf = options.keepProbConf
+
+    cpuCoreCnt = options.cpuCoreCnt
+    gpuNos = options.gpuNos
+    gpuMemFraction = options.gpuMemFraction
+    iteration = options.iteration
+    printProgressPerStepCnt = options.printProgressPerStepCnt
+
+    logLevel = options.logLevel
+
+    force = options.force
+
+    logLevel = logLevel.upper()
+
+    # TODO(20180630) check args
+
+    logging.info("initing")
+    rc = __init(logLevel, wkdir, force)
+    if rc != 0:
+        return rc
+
+    logging.info("loading data")
+    if cacheDir is not None and os.path.isdir(cacheDir):
+        logging.info("loading from cacheDir: %s" % cacheDir)
+        data = Data.loadFromCacheDir(cacheDir)
+    else:
+        if cacheDir is None:
+            logging.info("cache closed")
+        else:
+            logging.info("cacheDir not exist: %s" % cacheDir)
+        data = DataAdapter(
+            featureFrame3dDir=featureFrame3dDir, targetBehaviorDir=targetBehaviorDir,
+        ).run()
+        if cacheDir is not None:
+            logging.info("dumping to cacheDir: %s" % cacheDir)
+            os.mkdir(cacheDir)
+            Data.dumpToCacheDir(data, cacheDir)
+
+    logging.info("splitting data for train(%f) and test(%f)" % (trainExampleP, 1.0 - trainExampleP))
+    trainData, testData = data.splitTrainTest(trainExampleP)
+    del data
+
+    logging.info("training combined model")
+    Trainer(
+        trainData=trainData, testData=testData,
+        wkdir=wkdir,
+        lstmSize=lstmSize,
+        batchSizeConf=batchSizeConf, keepProbConf=keepProbConf,
+        cpuCoreCnt=cpuCoreCnt, gpuNos=gpuNos, gpuMemFraction=gpuMemFraction,
+        iteration=iteration, printProgressPerStepCnt=printProgressPerStepCnt,
+    ).run()
+
+    logging.info("cleaning")
+    __clean()
+
+    return 0
+
+
+def __init(logLevel, wkdir, force):
+    # useless
+    conf.logLevel = conf.LogLevel[logLevel]
+    conf.FeatureFrame3dDict.init()
+
+    if os.path.isdir(wkdir):
+        logging.warn("wkdir already exist: %s" % wkdir)
+        if not force:
+            return 1
+        logging.warn("delete and create")
+        shutil.rmtree(wkdir)
+    os.makedirs(wkdir)
+    return 0
+
+
+def __clean():
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ src/moduler/trainer/rnn_trainer.py
@@ -0,0 +1,340 @@
+# coding=utf-8
+import logging
+import os
+
+import tensorflow as tf
+
+import conf
+from src.moduler.moduler import Moduler
+
+
+class RnnTrainer(Moduler):
+    SUMMARIZE_IMAGE = False
+
+    def __init__(
+            self,
+            trainData=None, testData=None,
+            wkdir=None,
+            lstmSize=None,
+            batchSizeConf=None, keepProbConf=None,
+            cpuCoreCnt=None, gpuNos=None, gpuMemFraction=None,
+            iteration=None, printProgressPerStepCnt=None,
+    ):
+        super(RnnTrainer, self).__init__()
+        self.trainData = trainData
+        self.testData = testData
+        self.wkdir = wkdir
+        self.summaryDir = os.path.join(self.wkdir, "summary")
+
+        self.lstmSize = lstmSize
+
+        self.batchSizeConf = batchSizeConf
+        self.keepProbConf = keepProbConf
+
+        self.cpuCoreCnt = cpuCoreCnt
+        self.gpuNos = gpuNos
+        self.gpuMemFraction = gpuMemFraction
+        self.iteration = iteration
+        self.printProgressPerStepCnt = printProgressPerStepCnt
+
+    def run(self):
+        self.__init()
+        runConf, runInput, trainer, evaluator = self.__construct()
+
+        if self.gpuNos is not None:
+            assert self.gpuMemFraction is not None
+            os.environ["CUDA_VISIBLE_DEVICES"] = self.gpuNos
+            gpuOptions = tf.GPUOptions(
+                per_process_gpu_memory_fraction=self.gpuMemFraction,
+                allow_growth=True,
+            )
+            config = tf.ConfigProto(gpu_options=gpuOptions)
+        else:
+            assert self.cpuCoreCnt is not None
+            config = tf.ConfigProto(
+                device_count={"CPU": self.cpuCoreCnt},
+                inter_op_parallelism_threads=self.cpuCoreCnt,
+                intra_op_parallelism_threads=self.cpuCoreCnt,
+            )
+
+        logging.info("start to train.")
+        with tf.Session(config=config) as sess:
+            runConf.set(self.batchSizeConf, self.keepProbConf)
+            trainer.train(
+                sess, self.summaryDir,
+                runConf, runInput,
+                self.trainData, self.iteration, self.printProgressPerStepCnt,
+                self.testData, evaluator,
+            )
+
+            # TODO(20180722) dump model
+
+            logging.info("start to evaluate.")
+            evaluate_result = evaluator.evaluate(
+                sess,
+                runConf, runInput,
+                self.testData,
+            )
+            logging.info("final evaluate_result: %s" % (evaluate_result,))
+
+    def __init(self):
+        os.mkdir(self.summaryDir)
+
+    def __construct(self, ):
+        learnDayCnt = conf.TrainerDict.LEARN_DAY_CNT
+        ff3dShape = conf.FeatureFrame3dDict.FF3D_SHAPE
+        # fvLen = conf.FeatureVectorDict.FEATURE_VECTOR_LEN
+        fvLen = conf.FeatureFrame3dDict.FF3D_DEPTH * conf.FeatureFrame3dDict.FF3D_WIDTH
+
+        with tf.name_scope('conf') as _:
+            # must use [] as shape
+            batchSize = tf.placeholder(tf.int32, shape=[], name="batchSize")
+            keepProb = tf.placeholder(tf.float32, name="keepProb")
+
+            runConf = RnnTrainer._RunConf(batchSize, keepProb)
+
+        with tf.name_scope('input') as _:
+            # TODO(20180722) assert batchSize == x.shape[0] and batchSize == y_.shape[0]
+            x = tf.placeholder(tf.float32, shape=[None] + ([learnDayCnt] + ff3dShape), name='x')
+            y_ = tf.placeholder(tf.float32, shape=[None] + [1], name="y_")
+
+            tf.summary.histogram("realX", x)
+            tf.summary.histogram("realY", y_)
+
+            runInput = RnnTrainer._RunInput(x, y_)
+
+        with tf.name_scope('formatFv') as _:
+            assert fvLen == conf.FeatureFrame3dDict.FF3D_DEPTH * conf.FeatureFrame3dDict.FF3D_WIDTH
+            _xSlice = x[:, :, :, :1, :]
+            fv = tf.reshape(_xSlice, [batchSize * learnDayCnt] + [fvLen])
+
+            tf.summary.histogram("realFv", fv)
+
+        with tf.name_scope('purePredict') as _:
+            predictFv = self.__constructPurePredictScope(batchSize, fv, fvLen, learnDayCnt, keepProb)
+
+            tf.summary.histogram("predictFv", predictFv)
+
+        with tf.name_scope('targetBehaviorResolve') as _:
+            y = self.__constructtargetBehaviorResolve(predictFv, fvLen)
+
+            tf.summary.histogram("predictY", y)
+
+        with tf.name_scope('optimize') as _:
+            lossMse, lossRmse, lossMae, lossR2, lossRrmse, lossMape = self.__constructLoss(y, y_, batchSize)
+
+            with tf.name_scope("internalOptimize") as _:
+                logging.info("optimize by lossMse")
+                optimize = tf.train.AdamOptimizer().minimize(lossMse, name="optimize")
+
+            evaluator = RnnTrainer._Evaluator(lossMse, lossRmse, lossMae, lossR2, lossRrmse, lossMape)
+            trainer = RnnTrainer._Trainer(optimize)
+
+        return runConf, runInput, trainer, evaluator
+
+    def __constructPurePredictScope(self, batchSize, fv, fvLen, learnDayCnt, keepProb):
+        lstmSize = self.lstmSize
+
+        def lstmCell(lstmSize):
+            return tf.contrib.rnn.BasicLSTMCell(lstmSize)
+
+        # TODO(20180729) impl
+        def addVec2summary(vec, vecName):
+            if not RnnTrainer.SUMMARIZE_IMAGE:
+                return
+            # FIXME(20180730) covered old images with the same name
+            vecLen = vec.shape[1].value
+            vec = tf.reshape(vec, [-1] + [1, vecLen, 1])
+            tf.summary.image(vecName, vec)
+
+        with tf.name_scope('internalInput') as _:
+            addVec2summary(fv, "historyFv")
+
+            _mFv = tf.transpose(tf.reshape(fv, [batchSize] + [learnDayCnt, fvLen]), perm=[0, 2, 1], name="mFv")
+
+        with tf.name_scope('lstmCells') as _:
+            _cell = tf.contrib.rnn.MultiRNNCell([lstmCell(lstmSize) for _ in xrange(learnDayCnt)])
+            _initialState = _cell.zero_state(batchSize, tf.float32)
+            with tf.name_scope('dropput') as _:
+                _cell = tf.contrib.rnn.DropoutWrapper(_cell, output_keep_prob=keepProb)
+            # _output: shape[None, fvLen, lstmSize]
+            _output, _final_state = tf.nn.dynamic_rnn(_cell, _mFv, initial_state=_initialState)
+
+        with tf.name_scope('internalOutput') as _:
+            _output = tf.reshape(_output, [-1] + [fvLen * lstmSize])
+            _weight = tf.Variable(tf.truncated_normal([fvLen * lstmSize, fvLen], stddev=0.01))
+            _bia = tf.zeros([fvLen])
+            predictFv = tf.add(tf.matmul(_output, _weight), _bia, name="predictFv")
+
+            addVec2summary(fv, "predictFv")
+            tf.summary.histogram('weight', _weight)
+            tf.summary.histogram('bia', _bia)
+
+        return predictFv
+
+    def __constructLoss(self, y, y_, batchSize):
+        # cannot define name in the way below
+        # lossMse = tf.losses.mean_squared_error(y_, y)
+        _batchSizeF = tf.cast(batchSize, tf.float32)
+        with tf.name_scope("lossMse") as _:
+            _lossSe = tf.square(tf.subtract(y, y_))  # squared error
+            lossMse = tf.div(tf.reduce_sum(_lossSe), _batchSizeF, name="lossMse")  # mean squared error
+        with tf.name_scope("lossRmse") as _:
+            lossRmse = tf.sqrt(lossMse, name="lossRmse")  # root mean squared error
+        with tf.name_scope("lossMae") as _:
+            _lossAe = tf.abs(tf.subtract(y, y_))  # abstract error
+            lossMae = tf.div(tf.reduce_sum(_lossAe), _batchSizeF, name="lossMae")  # mean abstract error
+        with tf.name_scope("lossR2") as _:
+            _lossSr = tf.square(tf.subtract(tf.reduce_mean(y), y_))  # squared residual
+            lossR2 = tf.subtract(tf.ones([]), tf.div(tf.reduce_sum(_lossSe), tf.reduce_sum(_lossSr)),
+                                 name="lossR2")  # r2
+        with tf.name_scope("lossRrmse") as _:
+            lossRrmse = tf.div(lossRmse, tf.reduce_mean(y_), name="lossRrmse")
+        with tf.name_scope("lossMape") as _:
+            lossMape = tf.div(tf.reduce_sum(tf.div(_lossAe, y_ + 0.01)), _batchSizeF,
+                              name="lossMape")
+
+        tf.summary.scalar('lossMse', lossMse)
+        tf.summary.scalar('lossRmse', lossRmse)
+        tf.summary.scalar('lossMae', lossMae)
+        tf.summary.scalar('lossR2', lossR2)
+        tf.summary.scalar('lossRrmse', lossRrmse)
+        tf.summary.scalar('lossMape', lossMape)
+
+        return lossMse, lossRmse, lossMae, lossR2, lossRrmse, lossMape
+
+    def __constructtargetBehaviorResolve(self, predictFv, fvLen):
+        _weight = tf.Variable(tf.truncated_normal([fvLen, 1], stddev=0.01))
+        _bia = tf.zeros([1])
+        y = tf.add(tf.matmul(predictFv, _weight), _bia, name="y")
+
+        tf.summary.histogram("weight", _weight)
+        tf.summary.histogram("bia", _bia)
+
+        return y
+
+    class _RunConf(object):
+        def __init__(self, batchSize, keepProb):
+            super(RnnTrainer._RunConf, self).__init__()
+            self.batchSize = batchSize
+            self.keepProb = keepProb
+
+            self.batchSizeConf = None
+            self.keepProbConf = None
+
+        def set(self, batchSizeConf, keepProbConf):
+            self.batchSizeConf = batchSizeConf
+            self.keepProbConf = keepProbConf
+
+        def clear(self):
+            self.batchSizeConf = None
+            self.keepProbConf = None
+
+    class _RunInput(object):
+        def __init__(self, x, y_):
+            super(RnnTrainer._RunInput, self).__init__()
+            self.x = x
+            self.y_ = y_
+
+    class _Trainer(object):
+        def __init__(self, optimize):
+            super(RnnTrainer._Trainer, self).__init__()
+            self.optimize = optimize
+
+        def train(
+                self,
+                sess, summaryDir,
+                runConf, runInput,
+                trainData, iteration, printProgressPerStepCnt,
+                testData, evaluator,
+        ):
+            optimize = self.optimize
+
+            summaries = tf.summary.merge_all()
+            summaryWriter = tf.summary.FileWriter(logdir=summaryDir, graph=sess.graph)
+
+            runOptions = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+            runMetadata = tf.RunMetadata()
+
+            sess.run(tf.global_variables_initializer())
+            for stopNo in xrange(iteration):
+                trainCmBatch = trainData.randomSampleBatch(runConf.batchSizeConf)
+                # print progress
+                if stopNo % printProgressPerStepCnt == 0:
+                    trainEvlRs = evaluator.evaluate(
+                        sess,
+                        runConf, runInput,
+                        trainCmBatch,
+                    )
+                    testCmBatch = testData.randomSampleBatch(runConf.batchSizeConf)
+                    testEvlRs = evaluator.evaluate(
+                        sess,
+                        runConf, runInput,
+                        testCmBatch,
+                    )
+                    logging.info(
+                        "step %d before optimizing, training evaluate result: %s, testing evaluate result: %s"
+                        % (stopNo, trainEvlRs, testEvlRs))
+                # train
+                feedDict = {runConf.batchSize: runConf.batchSizeConf, runConf.keepProb: runConf.keepProbConf,
+                            runInput.x: trainCmBatch.learnMFf3ds, runInput.y_: trainCmBatch.predictTbs, }
+                sess.run(optimize, feed_dict=feedDict, options=runOptions, run_metadata=runMetadata)
+                # summarize
+                summariesV = summaries.eval(feed_dict=feedDict, session=sess)
+                summaryWriter.add_summary(summariesV, global_step=stopNo)
+
+            summaryWriter.close()
+
+    class _Evaluator(object):
+
+        def __init__(
+                self,
+                lossMse, lossRmse, lossMae, lossR2, lossRrmse, lossMape,
+        ):
+            super(RnnTrainer._Evaluator, self).__init__()
+            self.lossMse = lossMse
+            self.lossRmse = lossRmse
+            self.lossMae = lossMae
+            self.lossR2 = lossR2
+            self.lossRrmse = lossRrmse
+            self.lossMape = lossMape
+
+        def evaluate(
+                self,
+                sess,
+                runConf, runInput,
+                cmData,
+        ):
+            feedDict = {runConf.batchSize: cmData.exampleCnt, runConf.keepProb: 1.0,
+                        runInput.x: cmData.learnMFf3ds, runInput.y_: cmData.predictTbs, }
+            lossMseV, lossRmseV, lossMaeV, lossR2V, lossRrmseV, lossMapeV = sess.run(
+                [self.lossMse, self.lossRmse, self.lossMae, self.lossR2, self.lossRrmse, self.lossMape],
+                feed_dict=feedDict, )
+            return RnnTrainer._Evaluator.Result(
+                cmData.exampleCnt,
+                lossMseV, lossRmseV, lossMaeV, lossR2V, lossRrmseV, lossMapeV,
+            )
+
+        class Result(object):
+            def __init__(
+                    self,
+                    exampleCnt,
+                    lossMse, lossRmse, lossMae, lossR2, lossRrmse, lossMape,
+            ):
+                super(RnnTrainer._Evaluator.Result, self).__init__()
+                self.exampleCnt = exampleCnt
+
+                self.lossMse = lossMse
+                self.lossRmse = lossRmse
+                self.lossMae = lossMae
+                self.lossR2 = lossR2
+                self.lossRrmse = lossRrmse
+                self.lossMape = lossMape
+
+            def __str__(self):
+                return (
+                    "result {example_cnt: %d,"
+                    " lossMse: %.6f, lossRmse: %.6f, lossMae: %.6f, lossR2: %.6f, lossRrmse: %.6f, lossMape: %.6f}"
+                    % (self.exampleCnt,
+                       self.lossMse, self.lossRmse, self.lossMae, self.lossR2, self.lossRrmse, self.lossMape,)
+                )

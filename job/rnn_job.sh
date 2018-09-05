7bafe75a0142dc17c0a41c1ebb0c3b3abedb943c
forienlauo <244382021@qq.com>
2018-09-05 15:35:58 +0800

    Add job shell

---
+++ job/rnn_job.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -u
+set +e
+
+exp_cnt=5000
+p=0.8
+b=40
+I=500
+k=0.8
+G=0
+M=0.3
+S=1
+prefix=rnn
+suffix=c$exp_cnt-p$p-b$b-k$k-I$I
+log_dir=tmp/$prefix-log-$suffix
+train_dir=tmp/$prefix-train_wkdir-$suffix
+cache_dir=tmp/sampled_cache-$exp_cnt
+mkdir $log_dir
+python rnn_train.py -f null -t null -c $cache_dir -p $p -w $train_dir -b $b -k $k -G $G -M $M -I $I -S $S -F > $log_dir/`date +'%Y%m%d-%H%M%S'`.log 2>&1
+
